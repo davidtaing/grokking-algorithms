5f1b53b4a024a786af2cf5efb2f7ac42d1757dac
David Taing <adavidtaing@gmail.com>
2023-08-12 13:49:15 +1000

    feat(binary-search): add draft design with comments

--- binary-search.test.ts
+++ binary-search.test.ts
@@ -1,4 +1,24 @@
 function binarySearch(num: number, array: number[]): number | undefined {
+  /**
+   *  left = start index of array
+   *  right = end index of array
+   *
+   *  loop (forgot what invariant was) {
+   *    // find mid index
+   *    mid = right - left / 2  // note: need to use Math.floor since JS numbers are doubles
+   *
+   *    if array[mid] === num?
+   *      return mid index
+   *
+   *    if array[mid] < num?
+   *      move right index to mid
+   *    else // array[mid] > num
+   *      move left index to mid
+   *
+   *    move to next loop iteration
+   *  }
+   **/
+
   return 0;
 }
 
@@ -7,6 +27,11 @@ const testCases = [
     num: 1,
     array: [1, 2, 3],
     expected: 0,
+  },
+  {
+    num: 3,
+    array: [1, 2, 3],
+    expected: 2,
   },
 ];
 
