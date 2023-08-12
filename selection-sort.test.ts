e2d0faf79818263bfd3f5a85c83c102eb91e1547
David Taing <adavidtaing@gmail.com>
2023-08-12 21:20:27 +1000

    feat(selection-sort): add implemention and tests

--- selection-sort.test.ts
+++ selection-sort.test.ts
@@ -1,14 +1,28 @@
+/** */
 export function selectionSort(array: number[]) {
-  /**
-   * Init new empty array
-   *
-   * Nested loops
-   *  Find largest item and insert into results array
-   *
-   * Return results
-   */
+  if (array.length <= 1) return array;
+
+  for (let outer = 0; outer < array.length; outer++) {
+    let lowest = outer;
+
+    for (let inner = outer + 1; inner < array.length; inner++) {
+      if (array[inner] < array[lowest]) {
+        lowest = inner;
+      }
+    }
+
+    if (lowest !== outer) {
+      swap(array, outer, lowest);
+    }
+  }
 
   return array;
+}
+
+function swap(array: number[], firstIndex: number, secondIndex: number) {
+  const temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
 }
 
 const testCases = [
@@ -26,6 +40,16 @@ const testCases = [
     name: "handles already sorted array",
     array: [1, 2, 3],
     expected: [1, 2, 3],
+  },
+  {
+    name: "sorts reverse-ordered array",
+    array: [3, 2, 1],
+    expected: [1, 2, 3],
+  },
+  {
+    name: "sorts array containing integers and non-integers",
+    array: [4.5, 100, 1, 60, 120],
+    expected: [1, 4.5, 60, 100, 120],
   },
 ];
 
